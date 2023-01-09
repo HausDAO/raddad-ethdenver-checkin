130eddf4bc4f4a4e76cc351f4f9eea9065e8ed71
jordan <jordan.lesich@gmail.com>
2023-01-09 14:20:28 -0800

    fix envs

--- src/pages/Claim.tsx
+++ src/pages/Claim.tsx
@@ -1,14 +1,25 @@
 import { useDHConnect } from '@daohaus/connect';
 import { FormBuilder } from '@daohaus/form-builder';
 import { TXBuilder } from '@daohaus/tx-builder';
+import { Button } from '@daohaus/ui';
+import { useQueryClient } from 'react-query';
 
 import { FORM } from '../legos/forms';
 import { TARGET_DAO } from '../targetDAO';
 
 export const Claim = () => {
-  const { provider } = useDHConnect();
-
+  const client = useQueryClient();
   return (
-    <FormBuilder form={FORM.CHECK_IN} targetNetwork={TARGET_DAO.CHAIN_ID} />
+    <>
+      <Button onClick={() => client.clear()}>Clear Cache</Button>
+
+      <FormBuilder
+        form={FORM.CHECK_IN}
+        targetNetwork={TARGET_DAO.CHAIN_ID}
+        onSuccess={() => {
+          client.clear();
+        }}
+      />
+    </>
   );
 };
